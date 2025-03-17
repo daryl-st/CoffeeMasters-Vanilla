8f88457624990201ae779d8c0dfbaf330d19dc19
Dawit Genene <Dawit.spark@gmail.com>
2025-03-17 11:19:49 +0300

    Configure services and Routing

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
+import Store from "./services/Store.js";
+import API from "./services/API.js";
+import { loadData } from "./services/Menu.js";
+import Router from "./services/Router.js";
+
+window.app= {};
+app.store = Store;
+app.router = Router;
+
 // It's better to wait for the event for manipulation
 window.addEventListener("DOMContentLoaded", () => {
-    let nav = document.querySelector("nav");
-    nav.innerHTML = `
-        <h2> Hello DOM </h2>
-        <p> This is a string from the JS </p>
-    `
-})
+    loadData();
+    app.router.init();
+});
---
+++ services/Router.js
@@ -0,0 +1,62 @@
+const Router = {
+    init: () => {
+        document.querySelectorAll("a.navlink").forEach(link => {
+            link.addEventListener("click", event => {
+                event.preventDefault();
+                // const url = event.target.href;
+                const url = link.getAttribute("href"); // we are using closure
+
+                Router.go(url);
+            });
+        });
+
+        // Event Handler for URL changes
+        window.addEventListener("popstate", event => {
+            Router.go(event.state.route, false); // the event has access to the previously pushed state (route)
+        });                                      // and we don't want to add it to history this time
+
+        // Check the initial URL
+        Router.go(location.pathname);
+    },
+    go: (route, addToHistory=true) => {
+        console.log(`Going to ${route}`);
+
+        if (addToHistory) {
+            history.pushState({ route }, null, route);
+        }
+
+        let pageElement = null;
+        // in this case we are add and removing elements when switching (routing) between pages
+        // we can also use the hidden attribute to implement the same routing
+        switch(route) {
+            case "/":
+                pageElement = document.createElement("h1");
+                pageElement.textContent = "Home";
+                break;
+            case "/order":
+                pageElement = document.createElement("h1");
+                pageElement.textContent = "Order";
+                break;
+            default:
+                if (route.startsWith("/product-")) {
+                    pageElement = document.createElement("h1");
+                    pageElement.textContent = "Details";
+                    const paramID = route.substring(route.lastIndexOf("-") + 1);
+                    pageElement.dataset.id = paramID;
+                }
+        }
+
+        // if there is a pageElement...else maybe create client 404 page 
+        if (pageElement) {
+            const cache = document.querySelector("main");
+            // cache.children[0].remove();
+            cache.innerHTML = "";
+            cache.appendChild(pageElement);
+
+            window.scrollX = 0;
+            window.scrollY = 0;
+        }
+    }
+}
+
+export default Router;
